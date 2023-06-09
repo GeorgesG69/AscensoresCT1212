6b3d46f819244a88e589749f589ff8e6b9abe9d2
GeorgesG69 <132624679+GeorgesG69@users.noreply.github.com>
2023-06-09 13:09:23 -0400

    a

--- Main_app.py
+++ Main_app.py
@@ -21,7 +21,7 @@ def main():
     # Valores establecidos para el Sistema de Ascensores.
 
     Altura_entre_pisos = 3.5
-    Sótanos = 3
+    Sotanos = 3
     Pisos_Superiores = 85
 
     # Determinar el valor del contrapeso y cabina.
