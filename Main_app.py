ec231746b2ab765e47d2a89fb3d7e00631b86540
GeorgesG69 <132624679+GeorgesG69@users.noreply.github.com>
2023-06-09 13:06:18 -0400

    Add files via upload

---
+++ Main_app.py
@@ -0,0 +1,45 @@
+import numpy
+'''  
+Altura entre pisos= 3.5m
+El número de piso viene dado por:
+-sótanos: 3 pisos para todos
+-Nro de Pisos superiores:
+
+Edicaciones pisos>=30, hoteles
+Edificaciones pisos<30, edificios residenciales 
+
+George Galindez: 85
+
+-Pisos de área de ocupación de 750m2
+
+-Personas que ocupan pisos sótanos: 15/piso
+-Personas que ocupan cada nivel superior 35/piso
+'''
+
+def main():
+
+    # Valores establecidos para el Sistema de Ascensores.
+
+    Altura_entre_pisos = 3.5
+    Sótanos = 3
+    Pisos_Superiores = 85
+
+    # Determinar el valor del contrapeso y cabina.
+
+    def Valor_Contrapeso_Cabina():
+
+
+        pass
+
+    # Determinar cantidad de pasajeros según la norma COVENIN.
+
+    def Cantidad_Pasajeros():
+
+
+        pass
+
+    pass
+    
+
+if __name__ == "__main__":
+    main()
