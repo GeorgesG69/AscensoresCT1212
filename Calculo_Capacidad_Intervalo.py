d7caf50ab4e816c3978b74d0621779f059d7f226
GeorgesG69 <cddde123@gmail.com>
2023-06-14 09:01:32 -0400

    #

--- Calculo_Capacidad_Intervalo.py
+++ Calculo_Capacidad_Intervalo.py
@@ -58,7 +58,7 @@ TTV: Tiempo_Total_Viaje
     Pisos_Totales = Pisos_Servidos + Pisos_No_Servidos # (na)
 
     Nro_Ascensores = 6
-    Tamano_Puerta = 1100
+    Tamano_Puerta = 800
     Capacidad_Nominal_P = 19
     Velocidad_Nominal_Establecida = 10 #m/s
     Zona_expresa = True
