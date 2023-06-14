2f20f0faf9f3641eac7f8e1c3b35174a74a2050b
GeorgesG69 <cddde123@gmail.com>
2023-06-14 09:27:00 -0400

    #

--- Contrapeso_Cabina.py
+++ Contrapeso_Cabina.py
@@ -1,3 +1,6 @@
 # Determinación de los valores del Contrapeso y la Cabina según la normativa Covenin.
 
 Longitud_Fijacion_Cable = 0
+
+Contrapeso_Pared = 5 + Longitud_Fijacion_Cable*2/1000
+Carro_Contrapeso = 7 + Longitud_Fijacion_Cable*2/1000
