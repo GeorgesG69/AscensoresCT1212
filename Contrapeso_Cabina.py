07dbdac86899a0379f426a90d97d67b8dbdfe472
GeorgesG69 <cddde123@gmail.com>
2023-06-14 16:35:12 -0400

    #

--- Contrapeso_Cabina.py
+++ Contrapeso_Cabina.py
@@ -1,5 +1,8 @@
 # Determinación de los valores del Contrapeso y la Cabina según la normativa Covenin.
 
+import numpy
+import pandas
+
 Longitud_Fijacion_Cable = 0
 
 Contrapeso_Pared = 5 + Longitud_Fijacion_Cable*2/1000
