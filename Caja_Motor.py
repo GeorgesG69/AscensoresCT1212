f7f85c2ec640c7a3b796187a0aea1d895df233a1
GeorgesG69 <cddde123@gmail.com>
2023-06-14 21:11:30 -0400

    #

--- Caja_Motor.py
+++ Caja_Motor.py
@@ -6,4 +6,28 @@ la velocidad del motor a la de la polea del ascensor.
 
 6. Calculando el motor eléctrico con un 90% de eficiencia que gira a 3600RPM.
 '''
+import numpy
 
+RPM_Motor = 3600
+
+Velocidad_Tangencial = 10 #m/s
+
+Velocidad_Angular = 3600*2*numpy.pi/60
+
+Radio_Ideal = Velocidad_Tangencial/Velocidad_Angular
+
+
+# Cálculo de la caja de cambios:
+
+RPM_Salida = RPM_Motor - RPM_Motor*2/10
+
+Nro_Engranajes = RPM_Salida/RPM_Motor
+
+print(f"RPM = {RPM_Motor} [rpm]")
+print(f"Vtan = {Velocidad_Tangencial} [m/s]")
+print(f"Vang = {Velocidad_Angular} [rad/s]\n")
+
+print(f"Radio ideal = {Radio_Ideal} [m]\n")
+
+print(f"RPM de Salida = {RPM_Salida} [rpm]")
+print(f"Número de engranajes = {Nro_Engranajes}")
