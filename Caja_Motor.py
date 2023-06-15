1e0941e8c4405bbfce9381706842dc66b6d7f5a5
GeorgesG69 <cddde123@gmail.com>
2023-06-14 21:02:25 -0400

    #

---
+++ Caja_Motor.py
@@ -0,0 +1,9 @@
+#Cálculo de motor y caja de cambios:
+
+'''
+4. Considerando una caja de cambio mecánica, con un 80% de eficiencia que reduce
+la velocidad del motor a la de la polea del ascensor.
+
+6. Calculando el motor eléctrico con un 90% de eficiencia que gira a 3600RPM.
+'''
+
