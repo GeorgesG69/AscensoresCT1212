10a7bae664282b555c3fac4ccb09267f623b442a
GeorgesG69 <cddde123@gmail.com>
2023-06-14 20:58:47 -0400

    #

--- Contrapeso_Cabina.py
+++ Contrapeso_Cabina.py
@@ -7,7 +7,7 @@ import sys
 sys.path.append("C:\\Users\\Georges\\Desktop\\Mine\\Proyectos CT1212\\Ascensores\\AscensoresCT1212\\Calculo_Capacidad_Intervalo.py")
 from Calculo_Capacidad_Intervalo import main
 
-Res_Grupo_G, Zona_expresa_G,Pisos_Servidos_G,Pisos_No_Servidos_G,Pisos_Totales_G,Nro_Ascensores_G,Velocidad_Nominal_Establecida_G,ReferenciaV_Nom_G,Recorrido_Ppal_Super_G,Recorrido_Ppal_1PSuper_G,Recorrido_Superior_Servido_G,Tiempo_Apertira_cierre_G,Tiempo_Entrada_Salida_G,Tiempo_Viaje_Completo_G,Tiempo_Total_Viaje_G,Capacidad_Nominal_P_G,Personas_Viaje_G,Poblacion_Total_G,Paradas_Probables_G,Capacidad_Transporte_G,Intervalo_Probable_G,Tiempo_Llenado_G = main
+#Res_Grupo_G, Zona_expresa_G,Pisos_Servidos_G,Pisos_No_Servidos_G,Pisos_Totales_G,Nro_Ascensores_G,Velocidad_Nominal_Establecida_G,ReferenciaV_Nom_G,Recorrido_Ppal_Super_G,Recorrido_Ppal_1PSuper_G,Recorrido_Superior_Servido_G,Tiempo_Apertira_cierre_G,Tiempo_Entrada_Salida_G,Tiempo_Viaje_Completo_G,Tiempo_Total_Viaje_G,Capacidad_Nominal_P_G,Personas_Viaje_G,Poblacion_Total_G,Paradas_Probables_G,Capacidad_Transporte_G,Intervalo_Probable_G,Tiempo_Llenado_G = main
 
 #print(f"Velocidad nominal: {Velocidad_Nominal_Establecida}")
 
@@ -22,7 +22,7 @@ Carro_Contrapeso = 7 + Longitud_Fijacion_Cable*2/1000
 
 # Cables:
 
-Valor_T7 = 1
+Valor_T7 = 4
 
 Nro_Cables_Suspencion = 15
 
@@ -35,17 +35,17 @@ Factor_Seguridad = Valor_T7*Nro_Cables_Suspencion*Carga_Rotura_Cable/Peso_Cable
 # Rozamiento entre cables y polea:
 Coeficiente_M = 0.09
 
-Tipo_Garganta = "" #sin ñ
+Tipo_Garganta = "cuna" #sin ñ
 
 if Tipo_Garganta == "cuna":
 
-    Angulo_Mecanizado_Garganta = 0
+    Angulo_Mecanizado_Garganta = 0.785398
 
     Indice_Rozamiento = Coeficiente_M/numpy.sin(Angulo_Mecanizado_Garganta/2)
 
 elif Tipo_Garganta == "comb":
 
-    Angulo_Entralladura = 0
+    Angulo_Entralladura = 0.785398
 
     Indice_Rozamiento = 4*Coeficiente_M*(1-numpy.sin(Angulo_Entralladura/2))/(numpy.pi-Angulo_Entralladura-numpy.sin(Angulo_Entralladura))
 
@@ -78,8 +78,8 @@ Ref_Presion_esp = 125+40*Velocidad_Nominal
 
 # Tension:
 
-Tension_1 = 0
-Tension_2 = 0
+Tension_1 = Peso_estructura_carro*9.8
+Tension_2 = Peso_Contrapeso*9.8
 
 Ceoficiente_aceleracon = (9.8+1)/(9.8-1)
 Coeficiente_Forma_Garganta = 1
@@ -92,12 +92,12 @@ efa = numpy.exp(Indice_Rozamiento*ANgulo_Contacto)
 
 if Relacion_equilibrio_cables*Ceoficiente_aceleracon*Coeficiente_Forma_Garganta <= efa:
 
-    print(f"{Relacion_equilibrio_cables*Ceoficiente_aceleracon*Coeficiente_Forma_Garganta} <= {efa}")
+    print(f"Relación: {Relacion_equilibrio_cables*Ceoficiente_aceleracon*Coeficiente_Forma_Garganta} <= {efa}")
     print(f"Hay estabilidad en la instalación")
 
 elif Relacion_equilibrio_cables*Ceoficiente_aceleracon*Coeficiente_Forma_Garganta > efa:
 
-    print(f"{Relacion_equilibrio_cables*Ceoficiente_aceleracon*Coeficiente_Forma_Garganta} >= {efa}")
+    print(f"Relación: {Relacion_equilibrio_cables*Ceoficiente_aceleracon*Coeficiente_Forma_Garganta} >= {efa}")
     print(f"No hay estabilidad en la instalación")
 
 
@@ -111,5 +111,5 @@ print(f"Factor seguridad (F): {Factor_Seguridad} \n")
 
 print(f"Indice de rozamiento: {Indice_Rozamiento}")
 print(f"Referencia de la presión esperada: {Ref_Presion_esp}")
-print(f"Presion esperada: {Presion_esp}")
+print(f"Presion esperada: {Presion_esp} \n")
 
