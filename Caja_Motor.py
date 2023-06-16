1e4207294d9c110892bc06b1857ad55d3da81300
GeorgesG69 <cddde123@gmail.com>
2023-06-16 09:05:35 -0400

    #

--- Caja_Motor.py
+++ Caja_Motor.py
@@ -8,3 +8,22 @@ la velocidad del motor a la de la polea del ascensor.
 '''
 import numpy
 
+M_Cabina = 450 #kg
+M_Contrapeso = 1350 #kg
+Gravedad = 9.8
+
+Vtan_Polea = 10 #m/s
+
+Radio_Polea = 0
+
+Vang_Polea = Vtan_Polea/Radio_Polea
+
+Tension_1 = M_Cabina*Gravedad #N
+Tension_2 = M_Contrapeso*Gravedad #N
+
+
+Torque_Polea = Tension_2*Radio_Polea
+Potencia_Polea = Torque_Polea*Vang_Polea
+
+print(f"Tension 1: {Tension_1}")
+print(f"Tension 2: {Tension_2}")
