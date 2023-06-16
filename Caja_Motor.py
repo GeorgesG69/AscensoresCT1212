c7d9bcd5856ba61ea89e3c91bcc7f56bece13480
GeorgesG69 <cddde123@gmail.com>
2023-06-16 19:16:58 -0400

    #

--- Caja_Motor.py
+++ Caja_Motor.py
@@ -15,6 +15,7 @@ de = pd.read_excel("datos_ascensores.xls")
 
 def Calculos(fila):
 
+    grupo = de.iloc[fila, 0]
     M_Cabina = de.iloc[fila, 9]/2 #kg
     M_Contrapeso = de.iloc[fila, 9]*1.5/2 #kg
     Gravedad = 9.8
@@ -58,4 +59,48 @@ def Calculos(fila):
 
     print(f"Nro de engranajes: {Nro_Engranes} => {round(Nro_Engranes)} engranajes")
 
-Calculos(1)
+    return grupo, M_Contrapeso, M_Cabina, Tension, Radio_Polea, Torque_Polea, Potencia_Polea, Potencia_Caja, Potencia_Motor, Vang_Polea, round(Nro_Engranes)
+
+def guardar(grp, contrapeso, cabina, tension, radiop, torquep, potenciap, potenciac, potenciam, vangp, engranes):
+
+    df = pd.DataFrame({"Grupo" : [grp],
+                       "Contrapeso": [contrapeso],
+                       "Cabina" : [cabina],
+                       "Tension" : [tension],
+                       "Radio polea" : [radiop],
+                       "Torque polea" : [torquep],
+                       "Potencia polea" : [potenciap],
+                       "Potencia caja" : [potenciac],
+                       "Potencia motor" : [potenciam],
+                       "Vel ang polea" : [vangp],
+                       "Nro engranes" : [engranes]})
+    
+    df = df[["Grupo",
+             "Contrapeso",
+             "Cabina",
+             "Tension",
+             "Radio polea",
+             "Torque polea",
+             "Potencia polea",
+             "Potencia caja",
+             "Potencia motor",
+             "vel ang polea",
+             "Nro engranes"]]
+    
+    Escritor = pd.ExcelWriter(".\\Caja_Motor.xlsx")
+
+    df.to_excel(Escritor, f"Calculo {grp}", index=False)
+
+    Escritor._save()
+
+grpa, contrapesoa, cabinaa, tensiona, radiopa, torquepa, potenciapa, potenciaca, potenciama, vangpa, engranesa = Calculos(1)
+guardar(grpa, contrapesoa, cabinaa, tensiona, radiopa, torquepa, potenciapa, potenciaca, potenciama, vangpa, engranesa)
+
+grpB, contrapesoB, cabinaB, tensionB, radiopB, torquepB, potenciapB, potenciacB, potenciamB, vangpB, engranesB = Calculos(2)
+guardar(grpB, contrapesoB, cabinaB, tensionB, radiopB, torquepB, potenciapB, potenciacB, potenciamB, vangpB, engranesB)
+
+grpC, contrapesoC, cabinaC, tensionC, radiopC, torquepC, potenciapC, potenciacC, potenciamC, vangpC, engranesC = Calculos(3)
+guardar(grpC, contrapesoC, cabinaC, tensionC, radiopC, torquepC, potenciapC, potenciacC, potenciamC, vangpC, engranesC)
+
+grpD, contrapesoD, cabinaD, tensionD, radiopD, torquepD, potenciapD, potenciacD, potenciamD, vangpD, engranesD = Calculos(4)
+guardar(grpD, contrapesoD, cabinaD, tensionD, radiopD, torquepD, potenciapD, potenciacD, potenciamD, vangpD, engranesD)
