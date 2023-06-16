ca6ad6b4209340ff29a0a6e86e7fe63cf2522822
GeorgesG69 <cddde123@gmail.com>
2023-06-16 01:02:33 -0400

    #

--- Caja_Motor.py
+++ Caja_Motor.py
@@ -8,34 +8,3 @@ la velocidad del motor a la de la polea del ascensor.
 '''
 import numpy
 
-RPM_Motor = 3600
-
-Velocidad_Tangencial_Polea = 10 #m/s
-
-Velocidad_Angular_Motor = 3600*2*numpy.pi/60
-
-Radio_Polea = 2 #m
-
-Velocidad_Angular_Polea = Velocidad_Tangencial_Polea/Radio_Polea
-
-Factor_Reduccion = Velocidad_Angular_Polea/Velocidad_Angular_Motor
-
-
-# Cálculo de la caja de cambios:
-
-Velocidad_Salida = Velocidad_Angular_Motor*Factor_Reduccion
-
-
-
-Nro_Engranajes = 0
-
-print(f"RPM = {RPM_Motor} [rpm]")
-print(f"Vtan polea = {Velocidad_Tangencial_Polea} [m/s]")
-print(f"Velocidad angular de la polea = {Velocidad_Angular_Polea} [rad/s]")
-print(f"Vang motor= {Velocidad_Angular_Motor} [rad/s]\n")
-
-print(f"Radio de la polea = {Radio_Polea} [m]\n")
-print(f"Factor de reducción = {Factor_Reduccion} veces")
-
-print(f"Velocidad de Salida = {Velocidad_Salida} [rad/s]")
-print(f"Número de engranajes = {Nro_Engranajes}\n")
