a24e4ab34e3ef906391ddb7fd363dd48fc315831
GeorgesG69 <cddde123@gmail.com>
2023-06-16 09:53:01 -0400

    #

--- Caja_Motor.py
+++ Caja_Motor.py
@@ -14,7 +14,7 @@ Gravedad = 9.8
 
 Vtan_Polea = 10 #m/s
 
-Radio_Polea = 0
+Radio_Polea = 5
 
 Vang_Polea = Vtan_Polea/Radio_Polea
 
@@ -22,8 +22,29 @@ Tension_1 = M_Cabina*Gravedad #N
 Tension_2 = M_Contrapeso*Gravedad #N
 
 
-Torque_Polea = Tension_2*Radio_Polea
+Torque_Polea = Tension_1*Radio_Polea
+
 Potencia_Polea = Torque_Polea*Vang_Polea
 
+Potencia_Caja = 10*Potencia_Polea/8
+Potencia_Motor = 10*Potencia_Caja/9
+
+Torque_Motor = 162.5
+Vang_Motor = Potencia_Motor/Torque_Motor
+
+#Nro_Engranes = Vang_Motor/Vang_Polea
+
 print(f"Tension 1: {Tension_1}")
-print(f"Tension 2: {Tension_2}")
+print(f"Tension 2: {Tension_2}\n")
+
+print(f"Radio de la polea: {Radio_Polea}")
+print(f"Torque de la polea: {Torque_Polea}\n")
+
+print(f"Potencia de la polea: {Potencia_Polea}")
+print(f"Potencia de la caja: {Potencia_Caja}")
+print(f"Potencia del motor: {Potencia_Motor}\n")
+
+print(f"Velocidad angular de la polea: {Vang_Polea}")
+print(f"Velocidad angular del motor: {Vang_Motor} [rad/s] (377)\n")
+
+#print(f"Nro de engranajes: {Nro_Engranes}")
